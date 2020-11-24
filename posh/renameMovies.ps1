4949b24f59f0c3dfd9a141efd996da3da75040a3
Clarence Washington <washicjr@gmail.com>
2020-11-24 09:22:59 -0600

    Added funcGetTitleCase function to Rename Movies Script

--- posh/renameMovies.ps1
+++ posh/renameMovies.ps1
@@ -1,5 +1,11 @@
 #Requires -PSEdition Core
 
+function funcGetTitleCase($oldStr) {
+    $textInfo = (Get-Culture).TextInfo
+    $funcStr = $textInfo.ToTitleCase($oldStr.ToLower())
+    return $funcStr
+}
+
 Get-ChildItem "E:\.tmp\.torrent\downloads\completed" -Recurse -File -Exclude *.mp3,*.m4a,*.flac,*.mp4,*.lnk, *.jpg | Remove-Item
 Get-ChildItem "D:\sysutil\vm\vbox\hostshare" -Recurse -File -Exclude *.mp3,*.m4a,*.flac,*.mp4,*.lnk,*.jpg | Remove-Item
 
@@ -12,39 +18,41 @@ $arFolders | ForEach-Object {
     $searchSpec = $PSItem + '*.mp4'
     $files = Get-ChildItem $searchSpec -recurse
     ForEach ($file in $files){
-        $name = $file.name
-        $name = $name -replace '\d{2,4}\.', ''
-        $name = $name -replace '\d{3,4}p.*\.mp4', '.mp4'
-        $name = $name -replace '\.SD.*\.mp4', '.mp4'
-        $name = $name -replace 'XXX.*\.mp4', '.mp4'
-        $name = $name -replace 'KLEENEX.*\.mp4', '.mp4'
-        $name = $name -replace '\-', ''
-        $name = $name -replace '\s\s', ' '
-        $name = $name -replace '\[', ''
-        $name = $name -replace '\]', ''
-        $name = $name -replace '\(', ''
-        $name = $name -replace '\)', ''
-        $name = $name -replace '\.MP4\.mp4', '.mp4'
-        $name = $name -replace '\.', ' '
-        $name = $name -replace '\s\s', ' '
-        $name = $name -replace '\smp4', '.mp4'
-        $name = $name -replace 'ArchAngel', 'Archangel'
-        $name = $name -replace 'BBCParadise', 'BBC Paradise'
-        $name = $name -replace 'BigCockBully', 'Big Cock Bully'
-        $name = $name -replace 'BlackedRaw', 'Blacked Raw'
-        $name = $name -replace 'brothalovers', 'Brotha Lovers'
-        $name = $name -replace 'danejones', 'Dane Jones'
-        $name = $name -replace 'int3rracialpass', 'Interracial Pass'
-        $name = $name -replace 'interracialpass', 'Interracial Pass'
-        $name = $name -replace 'ManyVids', 'Many Vids'
-        $name = $name -replace 'julesjordan', 'Jules Jordan'
-        $name = $name -replace 'MonstersOfCock', 'Monsters of Cock'
-        $name = $name -replace 'PureTaboo', 'Pure Taboo'
-        $name = $name -replace 'sweetsinner', 'Sweet Sinner'
-        $name = $name -replace 'teensloveblackcocks', 'Teens Love Black Cocks'
-        $newLoc = $processedDir + $name
+        $strName = $file.name
+        $strName = $strName -replace '\d{2,4}\.', ''
+        $strName = $strName -replace '\d{3,4}p.*\.mp4', '.mp4'
+        $strName = $strName -replace '\.SD.*\.mp4', '.mp4'
+        $strName = $strName -replace 'XXX.*\.mp4', '.mp4'
+        $strName = $strName -replace 'KLEENEX.*\.mp4', '.mp4'
+        $strName = $strName -replace '\-', ''
+        $strName = $strName -replace '\s\s', ' '
+        $strName = $strName -replace '\[', ''
+        $strName = $strName -replace '\]', ''
+        $strName = $strName -replace '\(', ''
+        $strName = $strName -replace '\)', ''
+        $strName = $strName -replace '\.MP4\.mp4', '.mp4'
+        $strName = $strName -replace '\.', ' '
+        $strName = $strName -replace '\s\s', ' '
+        $strName = $strName -replace '\smp4', '.mp4'
+        $strName = $strName -replace 'ArchAngel', 'Archangel'
+        $strName = $strName -replace 'BBCParadise', 'BBC Paradise'
+        $strName = $strName -replace 'BigCockBully', 'Big Cock Bully'
+        $strName = $strName -replace 'BlackedRaw', 'Blacked Raw'
+        $strName = $strName -replace 'brothalovers', 'Brotha Lovers'
+        $strName = $strName -replace 'danejones', 'Dane Jones'
+        $strName = $strName -replace 'int3rracialpass', 'Interracial Pass'
+        $strName = $strName -replace 'interracialpass', 'Interracial Pass'
+        $strName = $strName -replace 'ManyVids', 'Many Vids'
+        $strName = $strName -replace 'julesjordan', 'Jules Jordan'
+        $strName = $strName -replace 'MonstersOfCock', 'Monsters of Cock'
+        $strName = $strName -replace 'PureTaboo', 'Pure Taboo'
+        $strName = $strName -replace 'sweetsinner', 'Sweet Sinner'
+        $strName = $strName -replace 'teensloveblackcocks', 'Teens Love Black Cocks'
+
+        $newName = funcGetTitleCase($strName)
+        $newLoc = $processedDir + $newName
         
-        Write-Host OLDNAME $file.name NEWNAME $name
+        Write-Host OLDNAME $file.name NEWNAME $newName
         Move-Item $file.fullname $newLoc
     }
 }
