2ed966f2680cfa782eefb52e81024cb715b3c25f
Clarence Washington <washicjr@gmail.com>
2021-03-31 18:31:26 -0500

    The script now backs up everything of interest

--- cmd/syncall.bat
+++ cmd/syncall.bat
@@ -6,4 +6,5 @@ robocopy c:\progra~2 z:\bkp\c\progra~2 /w:1 /r:1 /mir
 robocopy c:\programdata z:\bkp\c\programdata /w:1 /r:1 /mir /xj /xd C:\ProgramData\Microsoft\Windows\WER\ReportArchive /xd C:\ProgramData\Microsoft\Crypto
 robocopy P:\sysutil\bin z:\bkp\p\sysutil\bin /w:1 /r:1 /mir /XD $RECYCLE.BIN "System Volume Information"
 robocopy e:\ z:\bkp\e /w:1 /r:1 /mir /XD $RECYCLE.BIN "System Volume Information"
-
+robocopy c:\users\clarence z:\bkp\c\users\clarence /w:1 /r:1 /mir /xj
+robocopy d:\users\clarence z:\bkp\d\users\clarence /w:1 /r:1 /mir /xj
