2371f18573fb62ced470fa1e6abf5776c68bc2f1
Clarence Washington <washicjr@gmail.com>
2023-06-10 10:05:37 -0500

    modified syncsysutil

--- cmd/syncsysutil.bat
+++ cmd/syncsysutil.bat
@@ -1,3 +1,3 @@
 robocopy c:\sysutil z:\bkp\c\sysutil /NODCOPY /w:1 /r:1 /mir /xd c:\sysutil\tmp
 robocopy d:\sysutil z:\bkp\d\sysutil /NODCOPY /w:1 /r:1 /mir /xd d:\sysutil\tmp
-robocopy p:\sysutil z:\bkp\p\sysutil /NODCOPY /w:1 /r:1 /mir /xd p:\sysutil\tmp
+robocopy p: z:\bkp\p /NODCOPY /w:1 /r:1 /mir /xd p:\sysutil\tmp
