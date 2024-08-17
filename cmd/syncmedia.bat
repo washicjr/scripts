088b6fb3542db0d911a2d9661fb2ea22596b40e3
Clarence Washington <washicjr@gmail.com>
2024-08-17 13:56:04 -0500

    Make room on drives to backup music

--- cmd/syncmedia.bat
+++ cmd/syncmedia.bat
@@ -1,1 +1,2 @@
-robocopy e:\ z:\bkp\e /NODCOPY /w:1 /r:1 /mir /XD $RECYCLE.BIN "System Volume Information"
+robocopy e:\ z:\bkp\e /NODCOPY /w:1 /r:1 /mir /XD music $RECYCLE.BIN "System Volume Information"
+robocopy e:\music y:\bkp\e\music /NODCOPY /w:1 /r:1 /mir
