e06c741eb7e104097c2e31ddd823d00e43540bf7
Clarence Washington <washicjr@gmail.com>
2024-08-17 14:03:45 -0500

    Ensure music is backed up before video

--- cmd/syncmedia.bat
+++ cmd/syncmedia.bat
@@ -1,2 +1,2 @@
-robocopy e:\ z:\bkp\e /NODCOPY /w:1 /r:1 /mir /XD music $RECYCLE.BIN "System Volume Information"
 robocopy e:\music y:\bkp\e\music /NODCOPY /w:1 /r:1 /mir
+robocopy e:\ z:\bkp\e /NODCOPY /w:1 /r:1 /mir /XD music $RECYCLE.BIN "System Volume Information"
